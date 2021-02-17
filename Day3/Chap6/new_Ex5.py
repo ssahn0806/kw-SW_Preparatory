b0343f2b29a080aae85fe3c5227ee867f7848cb0
ssahn0806 <ssahn0806@naver.com>
2021-02-17 18:38:41 +0900

    modified some files

--- Day3/Chap6/new_Ex5.py
+++ Day3/Chap6/new_Ex5.py
@@ -1,7 +1,6 @@
-y = int(input("임의의 정수(1~9) 입력 : "))
+number = int(input('임의의 정수(1~9) 입력 : '))
 
-for i in range (1,y+1,1): # step size =1 은 생략하는게 일반적!
-    print()
-    for t in range (1,y+1,1):
-        if i >= t:
-            print(1*t,end = '')
+for i in range(1,number+1):
+    for j in range(1,i+1):
+        print(j,end='')
+    print('')
